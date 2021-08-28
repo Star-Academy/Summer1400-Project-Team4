68345ac28dc98afde1f12c1b48ba1155fdc1e032
= <saedi.behnam.336@gmail.com>
2021-08-28 19:41:15 +0430

    Remove extra html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
