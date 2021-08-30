4e6e876a1b9e27712006cefd61a0598722f198b4
Arash <arashyadegari0402@gmail.com>
2021-08-30 15:39:23 -0700

    add connection to database

---
+++ WebApplication2/WebApplication2/Services/ClientDbConnector.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace WebApplication2.Services
+{
+    public class ClientDbConnector : ISqlConnector
+
+    {
+        public SqlConnection Connect(string host, string database)
+        {
+            var connectionString = $"Server= {host} ; Database= {database}; Integrated Security=SSPI;";
+            var connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
