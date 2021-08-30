fb9bf4020f4bf9bf0dcb61aee6084a17e449d2c7
Abolfazl Ghalandary <79265051+abolfazlghalandary@users.noreply.github.com>
2021-08-30 18:53:33 +0430

    add pipeline section

---
+++ WebApplication2/WebApplication2/Services/JsonPipelineInterpreter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WebApplication2.Services.QueryServices;
+
+namespace WebApplication2.Services
+{
+    public class JsonPipelineInterpreter
+    {
+        public static IEnumerable<IQueryProcessor> QueriesList(string pipelineJson)
+        {
+            return null;
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Services/PiplineExecuter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using WebApplication2.Services.QueryServices;
+
+namespace WebApplication2.Services
+{
+    public class PiplineExecuter
+    {
+        public Object DataTable { get; set; }
+        public List<IQueryProcessor> QueriesList;
+    }
+}
