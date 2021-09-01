f9e93bacc138f07e529828d9c5263ce2533c6c6f
Abolfazl Ghalandary <79265051+abolfazlghalandary@users.noreply.github.com>
2021-09-01 11:53:39 +0430

    add api doc

--- WebApplication2/WebApplication2/Services/PiplineExecuter.cs
+++ WebApplication2/WebApplication2/Services/PiplineExecuter.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using WebApplication2.Services.QueryServices;
 
 namespace WebApplication2.Services
 {
     public class PiplineExecuter
     {
-        public Object DataTable { get; set; }
+        public object DataTable { get; set; }
         public List<IQueryProcessor> QueriesList;
     }
 }
