3ba33622cf8988299332d53f318c4c19d0d1863b
Arad Maleki <arad.m.t02@gmail.com>
2021-09-01 14:48:11 +0430

    added GetColumn function

--- WebApplication2/WebApplication2/Loader/CsvLoader.cs
+++ WebApplication2/WebApplication2/Loader/CsvLoader.cs
@@ -10,25 +10,27 @@ namespace WebApplication2.Loader
         private const string FilePath = "E://covid.csv";
         private string[] _firstLine;
         private string _tableName;
+        private StreamReader _streamReader;
         private string _newTableCommand;
         private string _importCsvCommand;
+        private string _fieldSeparator;
 
         public void Load()
         {
+            _fieldSeparator = ",";
             _tableName = "temp";
-            var streamReader = new StreamReader(FilePath);
-            _firstLine = streamReader.ReadLine()?.Split(',');
+            _streamReader = new StreamReader(FilePath);
+            _firstLine = _streamReader.ReadLine()?.Split(',');
             CreateNewTableCommand();
             ImportFromCsvCommand();
-            
         }
 
         private void ImportFromCsvCommand()
         {
             var stringBuilder = new StringBuilder();
-            stringBuilder.Append("BULK INSERT " + _tableName + "\n");
-            stringBuilder.Append("FROM " + FilePath + "\n");
-            stringBuilder.Append("WITH (\nFIRSTROW = 2,\nFIELDTERMINATOR = ',',\nROWTERMINATOR = '\\n',\nTABLOCK\n);");
+            stringBuilder.Append($"BULK INSERT {_tableName}\n");
+            stringBuilder.Append($"FROM {FilePath}\n");
+            stringBuilder.Append($"WITH (\nFIRSTROW = 2,\nFIELDTERMINATOR = '{_fieldSeparator}',\nROWTERMINATOR = '\\n',\nTABLOCK\n);");
             _importCsvCommand = stringBuilder.ToString();
         }
 
@@ -40,8 +42,22 @@ namespace WebApplication2.Loader
             {
                 stringBuilder.Append(s + " NVARCHAR(255),\n");
             }
+
             stringBuilder.Append(");");
             _newTableCommand = stringBuilder.ToString();
+        }
+
+
+        public string[] GetColumn(int columnNumber)
+        {
+            var column = new List<string>();
+            _streamReader = new StreamReader(FilePath);
+            while (!_streamReader.EndOfStream)
+            {
+                var row = _streamReader.ReadLine()?.Split(_fieldSeparator);
+                if (row != null) column.Add(row[columnNumber]);
+            }
+            return column.ToArray();
         }
     }
 }
