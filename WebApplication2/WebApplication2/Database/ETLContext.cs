e7234063302a7f5a03bc4fcb79936c4caaa7b2a1
Abolfazl Ghalandary <79265051+abolfazlghalandary@users.noreply.github.com>
2021-09-05 12:26:23 +0430

    configure efcore

---
+++ WebApplication2/WebApplication2/Database/ETLContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.models;
+
+namespace WebApplication2.Database
+{
+    public class EtlContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Pipeline> Pipelines { get; set; }
+        public DbSet<Dataset> Datasets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost\SQLExpress,1433;Database=ETL;Integrated Security=sspi;");
+        }
+    }
+}
--- WebApplication2/WebApplication2/models/Process.cs
+++ WebApplication2/WebApplication2/models/Process.cs
@@ -2,6 +2,7 @@
 {
     public class Process
     {
+        public int ProcessId { get; set; }
         public string Name { get; set; }
         public string Instruction { get; set; }
     }
