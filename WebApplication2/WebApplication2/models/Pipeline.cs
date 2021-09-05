6978c77ffccc58de43da86bcfff53d50ac2c1611
Abolfazl Ghalandary <79265051+abolfazlghalandary@users.noreply.github.com>
2021-09-05 12:08:39 +0430

    init models

--- WebApplication2/WebApplication2/models/Pipeline.cs
+++ WebApplication2/WebApplication2/models/Pipeline.cs
@@ -2,6 +2,7 @@
 {
     public class Pipeline
     {
+        public int PipelineId { get; set; }
         public string PipelineName { get; set; }
         public Process[] Processes { get; set; }
     }
---
+++ WebApplication2/WebApplication2/models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication2.models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public IEnumerable<Pipeline> Pipelines { get; set; }
+        public IEnumerable<Dataset> Datasets { get; set; }
+    }
+}
