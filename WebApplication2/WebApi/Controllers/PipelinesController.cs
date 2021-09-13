7be82d6c01304edd923aa817fe4bc3068e431771
Abolfazl Ghalandary <79265051+abolfazlghalandary@users.noreply.github.com>
2021-09-13 11:04:27 +0430

    improve PipelinesController

---
+++ WebApplication2/WebApi/Controllers/PipelinesController.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.models;
+using WebApi.Validations;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PipelinesController : ControllerBase
+    {
+        private Database _database;
+        private UserValidation _userValidation;
+
+        public PipelinesController(Database database, UserValidation userValidation)
+        {
+            _database = database;
+            _userValidation = userValidation;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNewPipeline([FromBody] Pipeline pipeline, [FromHeader] string token)
+        {
+            await _database.Pipelines.AddAsync(pipeline);
+            return Ok("پایپلاین با موفقیت اضافه شد");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletePipeline([FromRoute] int pipelineId, [FromHeader] string token)
+        {
+            var pipeline = _database.Pipelines.FirstOrDefaultAsync(p => p.PipelineId == pipelineId);
+            if (pipeline == null) return BadRequest();
+            _database.Pipelines.Remove(await pipeline);
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("{connectionId}")]
+        public async Task<ActionResult<Pipeline>> GetPipeline([FromRoute] int pipelineId,
+            [FromHeader] string token)
+        {
+            var pipeline = await _database.Pipelines.FirstOrDefaultAsync(p => p.PipelineId == pipelineId);
+            if (pipeline == null) return BadRequest();
+            return Ok(pipeline);
+        }
+
+
+    }
+}
--- WebApplication2/WebApi/models/Database.cs
+++ WebApplication2/WebApi/models/Database.cs
@@ -8,6 +8,7 @@ namespace WebApi.models
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Connection> Connections { get; set; }
+        public DbSet<Pipeline> Pipelines { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
