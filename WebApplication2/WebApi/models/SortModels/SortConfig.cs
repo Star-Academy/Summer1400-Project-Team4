95cadf5f27768b2334b46df0060fbbcbb78d3e40
Abolfazl Ghalandary <79265051+abolfazlghalandary@users.noreply.github.com>
2021-09-15 22:10:12 +0430

    handle Sort processing

---
+++ WebApplication2/WebApi/models/SortModels/SortConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace WebApi.models.SortModels
+{
+    public class SortConfig
+    {
+        public SortOrder[] Orders { get; set; }
+
+        public string GetFields()
+        {
+            return string.Join(", ", Orders.Select(o => o.GetSqlForm()));
+        }
+    }
+}
