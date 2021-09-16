9a4819967896df4382c15329d11f4a819ce7d5a6
= <saedi.behnam.336@gmail.com>
2021-09-16 13:03:16 +0430

    Get API address on startup

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,23 +4,34 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class ApiService {
-    API_URL = 'http://team4.somee.com/api/' as const;
-
+    DEFAULT_API_URL = 'http://team4.somee.com/api/' as const;
+    LOCAL_API_URL = 'localhost:443/api/' as const;
     HEADERS: { [header: string]: string } = {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-      'Cache-Control': 'no-cache, no-store, must-revalidate, post-check=0, pre-check=0' ,
+        'Cache-Control':
+            'no-cache, no-store, must-revalidate, post-check=0, pre-check=0',
         Pragma: 'no-cache',
         Expires: '0',
     };
 
-    constructor(private http: HttpClient) {}
+    apiUrl: string;
+
+    constructor(private http: HttpClient) {
+        this.apiUrl = prompt(
+            'آدرس API خود را وارد کنید به عنوان مثال:\n' +
+                this.DEFAULT_API_URL +
+                '\n' +
+                this.LOCAL_API_URL,
+            this.DEFAULT_API_URL
+        )!;
+    }
 
     get<T>(path: string, token?: string): Observable<T> {
         let headers = { ...this.HEADERS };
         if (token !== undefined) headers.token = token;
 
-        return this.http.get<T>(this.API_URL + path, {
+        return this.http.get<T>(this.apiUrl + path, {
             headers: headers,
         });
     }
@@ -29,7 +40,7 @@ export class ApiService {
         let headers = { ...this.HEADERS };
         if (token !== undefined) headers.token = token;
 
-        return this.http.post<T>(this.API_URL + path, body, {
+        return this.http.post<T>(this.apiUrl + path, body, {
             headers: headers,
         });
     }
@@ -38,7 +49,7 @@ export class ApiService {
         let headers = { ...this.HEADERS };
         if (token !== undefined) headers.token = token;
 
-        return this.http.put<T>(this.API_URL + path, body, {
+        return this.http.put<T>(this.apiUrl + path, body, {
             headers: headers,
         });
     }
@@ -47,7 +58,7 @@ export class ApiService {
         let headers = { ...this.HEADERS };
         if (token !== undefined) headers.token = token;
 
-        return this.http.delete<T>(this.API_URL + path, {
+        return this.http.delete<T>(this.apiUrl + path, {
             headers: headers,
         });
     }
